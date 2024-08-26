444c3988debae79db26bebc1d3a580b4a5cd388a
prsanthib <prasanthibammidi10@gmail.com>
2024-08-26 20:16:43 +0530

    day38 challenge completed

---
+++ dp/maxProdSubarray.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        if(nums.length == 0)
+        {
+            return 0;
+        }
+
+        int maxProd = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+
+        for(int i = 1 ; i < nums.length ; i++)
+        {
+            int n = nums[i];
+            //swap if n is negative because after multipliying 
+            //it gets reversed
+            if(n < 0)
+            {
+                int temp = currMax;
+                currMax = currMin;
+                currMin = temp;
+            }
+
+            currMax = Math.max(n , n*currMax);
+            currMin = Math.min(n , n*currMin);
+
+            maxProd = Math.max(maxProd,currMax);
+        }
+
+        return maxProd;
+    }
+}
